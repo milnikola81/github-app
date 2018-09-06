a403815d255a73af5c0a5c01dd144e0c7667650a
Nikola <milnikola81@gmail.com>
2018-09-06 18:41:12 +0200

    finished no.1

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import VueOnsen from 'vue-onsenui';
 // Onsen UI Styling and Icons
 require('onsenui/css-components-src/src/onsen-css-components.css');
 require('onsenui/css/onsenui.css');
+require('./assets/onsen-css-components.css')
+require('./assets/theme.css')
 
 import App from './App.vue';
 
