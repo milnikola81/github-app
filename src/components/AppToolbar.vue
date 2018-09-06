3941eb95c32c54e5773d8bb72a99288c8b323def
Nikola <milnikola81@gmail.com>
2018-09-06 18:58:39 +0200

    finished no.2

---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-ons-page>
+        <v-ons-toolbar>
+            <div class="left">
+                Left slot
+            </div>
+            <div class="center">
+                {{title}}
+            </div>
+            <div class="right">
+                Rigth slot
+            </div>
+        </v-ons-toolbar>
+    </v-ons-page>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'Github App'
+        }
+    }
+}
+</script>
+
