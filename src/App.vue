f9d534e25dd0c3dca5f4afa20a9a9edda643b164
Nikola <milnikola81@gmail.com>
2018-09-07 18:59:12 +0200

    finished no.7

--- src/App.vue
+++ src/App.vue
@@ -10,22 +10,25 @@
       <br>
       <div class="center">
         <AppSearch :query.sync="query" :placeholder="placeholder"/>
+        <br>
+        <v-ons-progress-circular indeterminate v-if="showLoader"></v-ons-progress-circular>
+        <v-ons-list>
+          <v-ons-list-header>
+            Repositories of: {{query}}
+          </v-ons-list-header>
+          <v-ons-list-item v-for="(repo, index) in repos" :key="index">
+            <div class="left">
+              <img class="list-item__thumbnail" :src="repo.owner.avatar_url" alt="">
+            </div>
+            <div class="center">
+              <span class="list-item__title">{{repo.name}}</span>
+              <span class="list-item__subtitle">{{repo.description}}</span>
+            </div>
+          </v-ons-list-item>
+        </v-ons-list>
       </div>
       <br>
-      <v-ons-list>
-        <v-ons-list-header>
-          Repositories of: {{query}}
-        </v-ons-list-header>
-        <v-ons-list-item v-for="(repo, index) in repos" :key="index">
-          <div class="left">
-            <img class="list-item__thumbnail" :src="repo.owner.avatar_url" alt="">
-          </div>
-          <div class="center">
-            <span class="list-item__title">{{repo.name}}</span>
-            <span class="list-item__subtitle">{{repo.description}}</span>
-          </div>
-        </v-ons-list-item>
-      </v-ons-list>
+
     </v-ons-page>
 </template>
 
@@ -46,7 +49,8 @@ export default {
       title: 'Github App',
       query: '',
       placeholder: 'Search...',
-      repos: ''
+      repos: '',
+      showLoader: false
     };
   },
   methods: {
@@ -56,10 +60,11 @@ export default {
   },
   watch: {
     query: debounce(function(newValue) {
+      this.showLoader = true
       gitHub.getRepos(newValue)
         .then((response) => {
           this.repos = response.data
-          console.log(response.data)
+          this.showLoader = false;
         })
     }, 500)
   }
